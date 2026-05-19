ac0e61d45a7be3df3034f0e8c90afe36c6806845
Florent Cardoso Crespy <florentcrsp@gmail.com>
2026-05-19 07:58:23 +0200

    Initial commit: backend setup

---
+++ backend/src/test/java/com/example/poketodo/PoketodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poketodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoketodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
