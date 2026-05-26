d07e0e3b935dc1a9f4cf78f39c07fa1d85a1dcf6
Florent Cardoso Crespy <florentcrsp@gmail.com>
2026-05-26 16:19:11 +0200

    feat: state todos avec interface Todo typée

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import './App.css'
-
-import { TodoItem } from './components/TodoItem' 
+import { useState } from 'react'
+import { TodoItem } from './components/TodoItem'
+import type { Todo } from './types/todo'
 
 function App() {
 
+  const [todos,setTodos] = useState<Todo[]>([
+    { id: 1, titre: "Apprendre React", done: true },
+    { id: 2, titre: "Faire la vaisselle", done: false },
+  ]);
+
   return (
      <div>
-       <TodoItem titre="Apprendre React" done={true} />
-       <TodoItem titre="Faire la vaisselle" done={false} />
-       <TodoItem titre="Faire le ménage" done={true} />
+       {todos.map((todo) => (
+         <TodoItem key={todo.id} titre={todo.titre} done={todo.done} />
+       ))}
      </div>
   )
 }
